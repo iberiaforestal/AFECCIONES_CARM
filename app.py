66746bc7e363ddb1ec310c0dcc5b13add95ed3cd
iberiaforestal <iberiaforestal.ing@gmail.com>
2025-11-07 11:37:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ shp_urls = {
 # Función para cargar shapefiles desde GitHub
 @st.cache_data
 def cargar_shapefile_desde_github(base_name):
-    base_url = "https://raw.githubusercontent.com/UDIFCARM/Afecciones_UDIF/main/CATASTRO/"
+    base_url ="https://raw.githubusercontent.com/iberiaforestal/AFECCIONES_CARM/main/CATASTRO/"
     exts = [".shp", ".shx", ".dbf", ".prj", ".cpg"]
     
     with tempfile.TemporaryDirectory() as tmpdir:
